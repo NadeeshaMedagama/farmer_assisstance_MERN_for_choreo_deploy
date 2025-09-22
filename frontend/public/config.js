eb7f4adfe186753e18faa058fef783d0c0fb84f2
NadeeshaMedagama <nadeeshamedaga@gmail.com>
2025-09-22 10:09:46 +0530

    Remove config.js from tracking and update .gitignore, update frontend index.html

--- frontend/public/config.js
+++
@@ -1,7 +0,0 @@
-window.config = {
-    apiUrl: 'https://bfdef01f-7fc1-46ea-af69-42279e15f710-dev-internal.e1-us-east-azure.internal.choreoapis.dev/farmer-assistance/express-mongo-backend/v1',
-    featureFlags: {
-        enableNewFeature: true,
-        enableExperimentalFeature: false,
-    },
-};
